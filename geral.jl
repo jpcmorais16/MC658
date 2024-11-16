b78f47bf891b6fcd912e894b5d152aa5c03aae24
jpcmorais16 <96194625+jpcmorais16@users.noreply.github.com>
2024-11-16 16:41:42 -0300

    multi-start local search implemented

--- geral.jl
+++ geral.jl
@@ -3,6 +3,7 @@
 # usados tanto na parte heurística como exata.
 #------------------------------------------------------------
 
+using Random
 
 function ConstructInitialSolution(G::RouteType)
 
@@ -13,24 +14,65 @@ function ConstructInitialSolution(G::RouteType)
     S[1] = 1
     S[2*G.K] = 1 + G.K
 
-    for i in 2:G.K-1
-        S[i] = i
-        S[i + 1] = i + G.K
-        i = i + 1
+    # Initialize random seed based on current time
+    Random.seed!(Int(time_ns()))
+    pickups = shuffle(collect(2:G.K))
+    
+    # Place pickups and their corresponding deliveries
+    j = 2
+    for i in 1:length(pickups)
+        current_pickup = pickups[i]
+        S[j] = current_pickup
+        S[j + 1] = current_pickup + G.K
+        j = j + 2
     end
-
     P[1] = 1
     D[1] = 2*G.K
 
-    for i in 2:G.K - 1
-        P[i] = i
-        D[i] = i + 1
+    for i in 2:2:2*G.K - 1
+        P[S[i]] = i
+        D[S[i]] = i + 1
     end
 
     S[1] = 1          
     S[2*G.K] = 1 + G.K 
 
     return S, P, D
+end
+
+function buildTree!(tree::Vector{Int}, array::Vector{Int}, times::Array{MyInt,2}, node::Int, s::Int, e::Int)
+    if s == e
+        tree[node] = times[array[s], array[s + 1]]
+    else
+        mid = div(s + e, 2)
+        left = 2 * node
+        right = 2 * node + 1
+        buildTree!(tree, array, times, left, s, mid)
+        buildTree!(tree, array, times, right, mid + 1, e)
+        tree[node] = tree[left] + tree[right] 
+    end
+end
+
+function queryTree(tree::Vector{Int}, S::Vector{MyInt}, i::Int, j::Int, s, e, node=1)
+
+    if i > e || j < s
+        return 0
+    end
+
+    if i <= s && e <= j
+        return tree[node]
+    end
+
+    mid = div(s + e, 2)
+    left_sum = queryTree(tree, S, i, j, s, mid, 2 * node)
+    right_sum = queryTree(tree, S, i, j, mid + 1, e, 2 * node + 1)
+    return left_sum + right_sum
+end
+
+function BuildTimeSegmentTree(G::RouteType, S::Vector{MyInt})
+    tree = Vector{Int}(undef, 8 * G.K)
+    buildTree!(tree, S, G.Time, 1, 1, 2 * G.K - 1)
+    return tree
 end
 
 function HeuristicCost(G::RouteType, S::Vector{MyInt}, P::Vector{MyInt}, D::Vector{MyInt})
@@ -42,17 +84,18 @@ function HeuristicCost(G::RouteType, S::Vector{MyInt}, P::Vector{MyInt}, D::Vect
         i = i + 1
     end
 
+    # Seg tree pra achar o somatório de G.Time[S[j],S[j+1]] para j de P[i] até D[i] - 1
+
+    tree = BuildTimeSegmentTree(G, S)
+
+
     for i in 1:G.K
         if (D[i] < P[i])
             TotalCost = Inf
             return TotalCost
         end
 
-        TotalTime = 0
-
-        for j in P[i]:D[i] - 1  
-            TotalTime = TotalTime + G.Time[S[j],S[j+1]]
-        end
+        TotalTime = queryTree(tree, S, P[i], D[i] - 1, 1, 2 * G.K - 1)
 
         if (TotalTime > G.Delta[i])
             TotalCost = Inf
@@ -66,37 +109,77 @@ function HeuristicCost(G::RouteType, S::Vector{MyInt}, P::Vector{MyInt}, D::Vect
 end
 
 
-function LocalSearchStep(G::RouteType, S::Vector{MyInt})
-
-    a = MyRand(2,2*G.K-1)
-    b = MyRand(2,2*G.K-1)
-    if (a==b)
-        continue
-    end
-    aux=S[a]
-    S[a]=S[b]
-    S[b]=aux
-end
-
-
-function TabuSearch(G::RouteType, MaxTimeSeconds::MyInt)
+function LocalSearch(G::RouteType, S::Vector{MyInt}, P::Vector{MyInt}, D::Vector{MyInt}, MaxTimeSeconds::MyInt)
     StartingTimeSeconds = time()
+    CurrBestSolutionValue = HeuristicCost(G, S, P, D)
+    println("Iniciado")
+    trocas = 0
 
-    S, P, D = ConstructInitialSolution(G)
-    
-    # Initialize best solution and its value
-    BestSolution = copy(S)
-    BestSolutionValue = HeuristicCost(G, S, P, D)
-    
-    # Initialize tabu list (using a simple array of moves)
-    tabuSize = min(20, G.K) # Typical tabu list size
-    tabuList = Vector{Tuple{MyInt,MyInt}}()
-    
-    while ((time()-StartingTimeSeconds) < MaxTimeSeconds)
-        
+    while (((time()-StartingTimeSeconds))<MaxTimeSeconds/50)
+        trocas = trocas + 1
+        a = MyRand(2,2 * G.K - 1)
+        b = MyRand(2,2 * G.K - 1)
+
+        if (a==b)
+            continue
+        end
+
+        # P[i] = x; S[x] = i
+        # D[i] = y; S[y] = i + G.K
+
+        a_pick = true
+        b_pick = true
+
+        if (S[a] <= G.K)
+            P[S[a]] = b
+        else
+            D[S[a] - G.K] = b
+            a_pick = false
+        end
+
+        if (S[b] <= G.K)
+            P[S[b]] = a
+        else
+            D[S[b] - G.K] = a
+            b_pick = false
+        end
+
+        aux=S[a]
+        S[a]=S[b]
+        S[b]=aux
+
+
+        ChangedSolutionValue = HeuristicCost(G, S, P, D)
+
+
+        if (ChangedSolutionValue < CurrBestSolutionValue)
+           # Solucao melhor. Mantem a troca e atualiza o valor desta solucao.
+          println("Solucao heuristica local melhorou de ",CurrBestSolutionValue," para ",ChangedSolutionValue,".")
+          println("Trocas: ", trocas)
+          trocas = 0
+           CurrBestSolutionValue = ChangedSolutionValue
+        else
+           # Troca nao melhorou. Desfaz a troca.
+            aux = S[a]
+            S[a] = S[b]
+            S[b] = aux
+            
+            if(a_pick)
+                P[S[a]] = a
+            else
+                D[S[a] - G.K] = a
+            end
+
+            if(b_pick)
+                P[S[b]] = b
+            else
+                D[S[b] - G.K] = b
+            end 
+        end
     end
-    
-    # Restore best solution found
-    copyto!(H, BestSolution)
-    return BestSolutionValue
+
+
+    return CurrBestSolutionValue
 end
+
+
--- heuristica.jl
+++ heuristica.jl
@@ -2,43 +2,35 @@
 #----------------------------------------------------------------
 function Heuristica(G::RouteType,MaxTimeSeconds::MyInt)
          
-	# Denote por D[i] o Delivery de i, e P[i] o Pick de i.
-	 # A solucao desta heuristica sempre coloca o "D[i]" logo depois do "P[i]",
-	 # por isso eh sempre viavel e neste caso, eh suficiente ter so' um vetor contendo a
-	 # sequencia de vertices em {2,3,...,K}.
-	 # Lembre que neste problema, o P[1] deve ser o primeiro no' e o D[1] o ultimo no'.
-	 # Dada uma sequencia de vertices em {2,3,...,K}, esta heuristica fica
-	 # sorteando duas posicoes distintas nesta sequencia, digamos "i" e "j"
-	 # e troca eles de posicao. Se o valor desta solucao melhorar, aceita. Caso contrario, nao
-	 # efetiva essa operacao e tenta uma proxima troca.
 
-    StartingTimeSeconds = time()
-    
-    # # H guarda a sequencia heuristica
-    # H::Vector{MyInt} =  Vector{MyInt}(undef,G.K)
+	StartingTimeSeconds = time()
+	BestSolutionValue = Inf
+	BestSolution = []
+	trocas = 0
 
-    # # Comeca com uma solucao dada por
-    # #           P[1]->P[2]->D[2]->P[3]->D[3]->...->P[K]->D[K]->D[1]
-    # for i in 1:G.K
-    #     H[i] = i
-    # end
 
-    H::Vector{MyInt} = ConstructInitialSolution(G)
+	while ((time()-StartingTimeSeconds) < MaxTimeSeconds)
 
-    CurrBestSolutionValue = DummyHeuristicCost(G,H)
+		S, P, D = ConstructInitialSolution(G)
 
-    TabuSearch(G, MaxTimeSeconds, H)
+		LocalBestSolutionValue = LocalSearch(G, S, P, D, MaxTimeSeconds)
 
-    # Itera enquanto tiver tempo
-    
-	if (CurrBestSolutionValue < G.BestValue)
-	   #println("Solucao heuristica melhorou de ",G.BestValue," para ",CurrBestSolutionValue,".")
-	   G.BestValue = CurrBestSolutionValue
-	   G.BestSol[1] = 1 # first node
-	   for i in 1:G.K-1
-	       G.BestSol[2*i] = H[i+1]
-	       G.BestSol[2*i+1] = H[i+1]+G.K
-	   end
-	   G.BestSol[2*G.K] = 1+G.K # last node is the target node
+		if (LocalBestSolutionValue < BestSolutionValue)
+			BestSolution = copy(S)
+			BestSolutionValue = LocalBestSolutionValue
+			println("Solucao global atualizada: ", BestSolutionValue)
+			trocas +=1
+		end
+
 	end
+
+	println("Melhor solucao encontrada: ", BestSolutionValue)
+	println("Trocas: ", trocas)
+
+	G.BestValue = BestSolutionValue
+
+	for i in 1:2*G.K
+		G.BestSol[i] = BestSolution[i]
+	end
+
 end
